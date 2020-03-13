318ca62c7de3a13a2e60f7a3b3684bd4114c8572
Jack McNamee <g00359656@gmit.ie>
2020-03-13 12:25:23 +0000

    Shunting Yard Algorithm

---
+++ shunting_yard.py
@@ -0,0 +1,55 @@
+# Jack McNamee - G00359656
+# The Shunting Yard Algorithm for regular expressions
+
+def shunting(infix):
+    """ Return the infix reg expression in postfix """
+
+    # convert the input to a stack-ish list and reverse
+    infix = list(infix)[::-1]
+
+    # operator stack
+    operators = []
+
+    # output list
+    postfix = []
+
+    # operator precedence
+    prec = {'*':100, '.':80, '|':60, ')':40, '(':20}
+
+    # loop through the input one char at a time
+    while infix:
+        # pop a char from the input
+        char = infix.pop()
+
+        # decide what to do based on the char
+        if char == '(':
+            # push an open brack to operators[]
+            operators.append(char)
+
+        elif char == ')':
+            # pop operators[] until an open bracket is found
+            while opers[-1] != '(':
+                postfix.append(operators.pop())
+            # remove the open bracket
+            operators.pop()
+
+        elif char in prec:
+            # push any operators on operators[]
+            # with higher precedence to the output
+            while operators and prec[char] < prec[operators[-1]]:
+                postfix.append(opers.pop())
+            # push char to operators[]
+            operators.append(c)
+
+        else:
+            # push the char to the output(postfix)
+            postfix.append(char)
+
+    # pop all operators to the output
+    while operators:
+        postfix.append(operators.pop())
+
+    # convert postfix to a string
+    return ''.join(postfix)
+
+

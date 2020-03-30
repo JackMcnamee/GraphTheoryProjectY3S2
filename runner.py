c3293385f986b1259d3bac8fc21d03d158afccd5
Jack McNamee <g00359656@gmit.ie>
2020-03-30 11:11:15 +0000

    Allow user to enter a regex and string

--- runner.py
+++ runner.py
@@ -40,5 +40,31 @@ def match(regex, s):
     # check if NFA matches the string
     return nfa.accept in current
 
-print(match("a.b|b*", "bbbb"))
+# welcoming message to user
+print("Welcome to my Graph Theory Project")
+print("Here you can check if a regular expression matches a string")
 
+# initialize exitProgram as empty string
+exitProgram = ''
+
+while exitProgram != 'quit':
+    # while loop that allows user to continue using
+    # this program until they type 'quit'
+
+    # ask user for a regular expression
+    print("Please enter your regular expression here: ")
+    regularExpression = input()
+    
+    # ask user for a string
+    print("Please enter your test string here: ")
+    testString = input()
+
+    # prints true or false
+    print(match(regularExpression, testString))
+
+    # ask user if they want to continue or exit
+    print("Enter 1 to try again or quit to exit: ")
+    exitProgram = input()
+
+# message for user when they are finished (entered 'quit')
+print("Goodbye")

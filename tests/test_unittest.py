d4077740f68e39333965db6f601f3219cd8e969e
Jack McNamee <g00359656@gmit.ie>
2020-05-05 09:51:47 +0000

    Testing completed

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -1,3 +1,6 @@
+# Jack McNamee
+# Testing class for this program using unittest
+
 import unittest
 
 import sys
@@ -5,23 +8,35 @@ sys.path.append('../')
 
 from match import match
 
-
-class Test(unittest.TestCase):
-    def test_match_one(self):
-        '''
-        Test that the regex and string
-        are equal
-        '''
-        regex1 = "a.b"
-        string1 = "b"
-        result = match(regex1, string1)
-        self.assertFalse(result)
-
-    def test_match_two(self):
-        regex2 = "a.b"
-        string2 = "b"
-        result = match(regex2, string2)
+'''
+Test that the output of matching 
+regex and string is true (assertTrue)
+'''
+class TestClassA(unittest.TestCase):
+    def test_match(self):
+        regex = "(a*.b*)|a.b"
+        string = "aaabbb"
+        result = match(regex, string)
         self.assertTrue(result)
+        pass
 
+class TestClassB(unittest.TestCase):
+    def test_match(self):
+        regex = "a+.b*"
+        string = "aaabbb"
+        result = match(regex, string)
+        self.assertTrue(result)
+        pass
+
+class TestClassC(unittest.TestCase):
+    def test_match(self):
+        regex = "a*|b?"
+        string = "aaabbb"
+        result = match(regex, string)
+        self.assertTrue(result)
+        pass
+
+# runs all tests
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromModule(sys.modules[__name__])
+    unittest.TextTestRunner(vebosity=2).run(suite)
